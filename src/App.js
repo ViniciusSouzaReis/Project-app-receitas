bb2544de19bd4e16ea69283da14a1f50378c53f4
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2022-09-23 16:03:59 -0300

    Configurações iniciais

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import UserProvider from './Api-Context/providers/UserProvider';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <UserProvider>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/meals" component={ Login } />
+          <Route exact path="/drinks" component={ Login } />
+          <Route exact path="/meals/:id" component={ Login } />
+          <Route exact path="/drinks/:id" component={ Login } />
+          <Route exact path="/meals/:id/in-progress" component={ Login } />
+          <Route exact path="/drinks/:id/in-progress" component={ Login } />
+          <Route exact path="/profile" component={ Login } />
+          <Route exact path="/done-recipes" component={ Login } />
+          <Route exact path="/favorite-recipes" component={ Login } />
+        </Switch>
+      </UserProvider>
+    </BrowserRouter>
   );
 }
 
