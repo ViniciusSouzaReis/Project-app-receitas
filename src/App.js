703e096fcc723c42c333a22df41972a3f3228160
Luis Rowe <luisfelipe.apoloniorowe@gmail.com>
2022-09-23 20:57:09 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import './App.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import UserProvider from './Api-Context/providers/UserProvider';
-import SearchBar from './components/SearchBar';
 import Login from './pages/Login';
 import Recipes from './pages/Recipes';
 
