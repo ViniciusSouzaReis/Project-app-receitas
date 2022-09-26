e63e31a671a5a20b57e2f3cb7690faa7fa3e8206
Luis Rowe <luisfelipe.apoloniorowe@gmail.com>
2022-09-26 17:37:12 -0300

    Iniciando os cards

---
+++ src/components/Categories.js
@@ -0,0 +1,62 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import RecipesContext from '../Api-Context/contexts/RecipesContext';
+
+function Categories() {
+  const { location: { pathname } } = useHistory();
+  const [categories, setCategories] = useState([]);
+  const { apiFetch } = useContext(RecipesContext);
+
+  useEffect(() => {
+    const fetchApiCategory = async (type) => {
+      let URL = '';
+      let typeReturn = '';
+      if (type === '/meals') {
+        URL = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+        typeReturn = 'categories';
+      } else {
+        URL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+        typeReturn = 'drinks';
+      }
+      const response = await fetch(URL);
+      const categoriesApi = await response.json();
+      const numberCategoryMax = 5;
+      const categoriesFive = categoriesApi[typeReturn]
+        .filter((element, index) => index < numberCategoryMax);
+      setCategories(categoriesFive);
+    };
+    fetchApiCategory(pathname);
+  }, [pathname]);
+
+  const handleClick = (name) => {
+    if (pathname === '/meals') {
+      apiFetch('meal', 'categories', name);
+    } else if (pathname === '/drinks') {
+      apiFetch('cocktail');
+    }
+  };
+
+  return (
+    <ul>
+      {categories.map((category) => (
+        <li
+          key={ category.strCategory }
+          data-testid={ `${category.strCategory}-category-filter` }
+          role="presentation"
+          name={ category.strCategory }
+          onClick={ () => handleClick(category.strCategory) }
+        >
+          {category.strCategory}
+        </li>))}
+      <li
+        data-testid="All-category-filter"
+        role="presentation"
+        onClick={ handleClick }
+      >
+        All
+      </li>
+    </ul>
+  );
+}
+
+export default Categories;
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import RecipesContext from '../Api-Context/contexts/RecipesContext';
+import CardRecipies from '../components/CardRecipies';
+import Categories from '../components/Categories';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Recipes() {
+  const { apiReturn } = useContext(RecipesContext);
   return (
     <div>
       <div><Header /></div>
+      <main>
+        <Categories />
+        {apiReturn.length > 0 && <CardRecipies />}
+      </main>
       <div><Footer /></div>
     </div>
   );
