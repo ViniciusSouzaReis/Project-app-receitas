0d6e1884cff7d1e7573aa4a850a7132ec4e8c82d
Luis Rowe <luisfelipe.apoloniorowe@gmail.com>
2022-09-27 17:55:07 -0300

    Req 44 e 45

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -3,10 +3,37 @@ import Header from '../components/Header';
 import HorizontalCard from '../components/HorizontalCard';
 
 function DoneRecipes() {
+  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+  const handleClick = () => {
+    console.log('oi');
+  };
   return (
     <div>
       <Header />
-      <HorizontalCard />
+      <div>
+        <button
+          type="button"
+          onClick={ handleClick }
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </button>
+        <button
+          type="button"
+          onClick={ handleClick }
+          data-testid="filter-by-meal-btn"
+        >
+          Meals
+        </button>
+        <button
+          type="button"
+          onClick={ handleClick }
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
+        </button>
+      </div>
+      {doneRecipes && <HorizontalCard />}
     </div>
   );
 }
