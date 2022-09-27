a4552f04b6dca39d1a84ad8271d32112c94c6658
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2022-09-27 16:14:45 -0300

    Requisitos 28,29,30 e 31

--- src/pages/RecipesDetails.js
+++ src/pages/RecipesDetails.js
@@ -1,18 +1,61 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import RecipesContext from '../Api-Context/contexts/RecipesContext';
 import BodyDetail from '../components/BodyDetail';
 import RecomedationDetail from '../components/RecomedationDetail';
+import { readInProgressRecipes } from '../services/inProgressRecipesLocalStorage';
 
 let OK_FETCH = true;
+const IN_PROGRESS_RECIPES = 'inProgressRecipes';
+const DONE_RECIPES = 'doneRecipes';
 
 function RecipesDetails() {
   const { apiFetch, apiReturn } = useContext(RecipesContext);
-  const { location: { pathname } } = useHistory();
+  const { push, location: { pathname } } = useHistory();
+  const [storageInProgress, setInProgress] = useState([]);
+  const [doneStorage, setDoneStorage] = useState([]);
+  const [existInProgress, setExistInProgress] = useState(false);
+  const [existDoneRecipe, setExistDoneRecipe] = useState(false);
   const arrayPath = pathname.split('/');
   // console.log(arrayPath[1]);
   // console.log(apiReturn);
   // console.log(OK_FETCH);
+
+  useEffect(() => {
+    if (!JSON.parse(localStorage.getItem(IN_PROGRESS_RECIPES))) {
+      localStorage.setItem(IN_PROGRESS_RECIPES, JSON.stringify({}));
+    }
+    if (!JSON.parse(localStorage.getItem(DONE_RECIPES))) {
+      localStorage.setItem(DONE_RECIPES, JSON.stringify([]));
+    }
+    setInProgress(readInProgressRecipes(IN_PROGRESS_RECIPES));
+    setDoneStorage(readInProgressRecipes(DONE_RECIPES));
+  }, []);
+
+  useEffect(() => {
+    const compareRecipes = () => {
+      if (doneStorage.length > 0) {
+        setExistDoneRecipe(doneStorage.some((element) => element.id === arrayPath[2]));
+      }
+    };
+    const compareInProgress = () => {
+      const { drinks, meals } = storageInProgress;
+      if (meals) {
+        const meal = Object.keys(meals);
+        if (arrayPath[1] === 'meals') {
+          setExistInProgress(meal.some((element) => element === arrayPath[2]));
+        }
+      }
+      if (drinks) {
+        const drink = Object.keys(drinks);
+        if (arrayPath[1] === 'drinks') {
+          setExistInProgress(drink.some((element) => element === arrayPath[2]));
+        }
+      }
+    };
+    compareRecipes();
+    compareInProgress();
+  }, [arrayPath, doneStorage, storageInProgress]);
 
   useEffect(() => {
     if (OK_FETCH) {
@@ -26,6 +69,12 @@ function RecipesDetails() {
   }, [apiFetch, arrayPath]);
 
   useEffect(() => () => { OK_FETCH = true; }, []);
+
+  const handleClick = () => {
+    // console.log(pathname);
+    // addInProgressRecipes(IN_PROGRESS_RECIPES, apiReturn[0]);
+    push(`${pathname}/in-progress`);
+  };
 
   return (
     <div style={ { display: 'flex', flexDirection: 'column', alignItems: 'center' } }>
@@ -46,15 +95,28 @@ function RecipesDetails() {
             />
           )
       )}
-      <button
-        type="button"
-        data-testid="start-recipe-btn"
-        style={ { position: 'fixed', marginBottom: '0px', marginTop: '610px' } }
-        id="btn-start-recipes"
-      >
-        Start Recipe
-      </button>
-
+      {(!existDoneRecipe) && (
+        <button
+          type="button"
+          data-testid="start-recipe-btn"
+          style={ { position: 'fixed', marginBottom: '0px', marginTop: '610px' } }
+          id="btn-start-recipes"
+          onClick={ handleClick }
+        >
+          {(existInProgress) ? ('Continue Recipe') : ('Start Recipe')}
+        </button>
+      )}
+      {/* {(existInProgress) && (
+        <button
+          type="button"
+          data-testid="start-recipe-btn"
+          style={ { position: 'fixed', marginBottom: '0px', marginTop: '610px' } }
+          id="btn-start-recipes"
+          onClick={ handleClick }
+        >
+          Continue Recipe
+        </button>
+      )} */}
       <RecomedationDetail />
     </div>
   );
--- src/services/inProgressRecipesLocalStorage.js
+++ src/services/inProgressRecipesLocalStorage.js
@@ -1,14 +1,52 @@
-const IN_PROGRESS_RECIPES = 'inProgressRecipes';
+// const IN_PROGRESS_RECIPES = 'inProgressRecipes';
+// const TIMEOUT = 500;
+// const SUCCESS_STATUS = 'OK';
+
+// if (!JSON.parse(localStorage.getItem(IN_PROGRESS_RECIPES))) {
+//   localStorage.setItem(IN_PROGRESS_RECIPES, JSON.stringify([]));
+// }
+// const readInProgressRecipes = () => JSON.parse(localStorage.getItem(IN_PROGRESS_RECIPES));
+
+// const saveInProgressRecipes = (inProgressRecipes) => localStorage
+//   .setItem(IN_PROGRESS_RECIPES, JSON.stringify(inProgressRecipes));
+
+// const simulateRequest = (response) => (callback) => {
+//   setTimeout(() => {
+//     callback(response);
+//   }, TIMEOUT);
+// };
+
+// export const getInProgressRecipes = () => new Promise((resolve) => {
+//   const doneRecipes = readInProgressRecipes();
+//   simulateRequest(doneRecipes)(resolve);
+// });
+
+// export const addInProgressRecipes = (song) => new Promise((resolve) => {
+//   if (song) {
+//     const favoriteSongs = readInProgressRecipes();
+//     saveInProgressRecipes([...favoriteSongs, song]);
+//   }
+//   simulateRequest(SUCCESS_STATUS)(resolve);
+// });
+
+// export const removeDoneRecipes = (song) => new Promise((resolve) => {
+//   const favoriteSongs = readInProgressRecipes();
+//   saveInProgressRecipes(favoriteSongs.filter((s) => s.trackId !== song.trackId));
+//   simulateRequest(SUCCESS_STATUS)(resolve);
+// });
+
+// const IN_PROGRESS_RECIPES = 'inProgressRecipes';
 const TIMEOUT = 500;
 const SUCCESS_STATUS = 'OK';
 
-if (!JSON.parse(localStorage.getItem(IN_PROGRESS_RECIPES))) {
-  localStorage.setItem(IN_PROGRESS_RECIPES, JSON.stringify([]));
-}
-const readInProgressRecipes = () => JSON.parse(localStorage.getItem(IN_PROGRESS_RECIPES));
+// if (!JSON.parse(localStorage.getItem(IN_PROGRESS_RECIPES))) {
+//   localStorage.setItem(IN_PROGRESS_RECIPES, JSON.stringify([]));
+// }
 
-const saveInProgressRecipes = (inProgressRecipes) => localStorage
-  .setItem(IN_PROGRESS_RECIPES, JSON.stringify(inProgressRecipes));
+export const readInProgressRecipes = (key) => JSON.parse(localStorage.getItem(key));
+
+export const saveInProgressRecipes = (key, value) => localStorage
+  .setItem(key, JSON.stringify(value));
 
 const simulateRequest = (response) => (callback) => {
   setTimeout(() => {
@@ -16,21 +54,21 @@ const simulateRequest = (response) => (callback) => {
   }, TIMEOUT);
 };
 
-export const getInProgressRecipes = () => new Promise((resolve) => {
-  const doneRecipes = readInProgressRecipes();
+export const getInProgressRecipes = (key) => new Promise((resolve) => {
+  const doneRecipes = readInProgressRecipes(key);
   simulateRequest(doneRecipes)(resolve);
 });
 
-export const addInProgressRecipes = (song) => new Promise((resolve) => {
-  if (song) {
-    const favoriteSongs = readInProgressRecipes();
-    saveInProgressRecipes([...favoriteSongs, song]);
+export const addInProgressRecipes = (key, value) => new Promise((resolve) => {
+  if (value) {
+    const prevValue = readInProgressRecipes(key);
+    saveInProgressRecipes(key, [...prevValue, value]);
   }
   simulateRequest(SUCCESS_STATUS)(resolve);
 });
 
-export const removeDoneRecipes = (song) => new Promise((resolve) => {
-  const favoriteSongs = readInProgressRecipes();
-  saveInProgressRecipes(favoriteSongs.filter((s) => s.trackId !== song.trackId));
+export const removeDoneRecipes = (key) => new Promise((resolve) => {
+  const favoriteSongs = readInProgressRecipes(key);
+  saveInProgressRecipes(favoriteSongs.filter((s) => s.trackId !== key.trackId));
   simulateRequest(SUCCESS_STATUS)(resolve);
 });
